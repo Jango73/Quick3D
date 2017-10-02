8a308ce91e243e94a577eaac07ec605ee1b4b819
Guillaume Darier <guillaumed00@gmail.com>
2017-10-02 08:39:29 +0200

    Minor fixes

--- Quick3D/Source/CGeolocalized.cpp
+++ Quick3D/Source/CGeolocalized.cpp
@@ -25,6 +25,14 @@ CGeolocalized::~CGeolocalized()
 
 void CGeolocalized::setGeoloc(CGeoloc gGeoloc)
 {
+    // Coordinates are clipped between -180.0 and +180.0 degrees
+    gGeoloc.Latitude = Math::Angles::clipAngleDegreePIMinusPI(gGeoloc.Latitude);
+    gGeoloc.Longitude = Math::Angles::clipAngleDegreePIMinusPI(gGeoloc.Longitude);
+
+    // Latitude is clipped between -89.9 and +89.9 degrees
+    gGeoloc.Latitude = Math::Angles::clipDouble(gGeoloc.Latitude, -89.9, 89.9);
+    gGeoloc.Altitude = Math::Angles::clipDouble(gGeoloc.Altitude, -20000.0, 12000000.0);
+
     m_gGeoloc = gGeoloc;
 }
 
